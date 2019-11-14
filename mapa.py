303c820b3186327d2b1c82ed6a2cc6e7853d3165
sebideth <sebaznarez@hotmail.com>
2019-11-14 18:35:08 -0300

    mapa.py terminado(creo)

--- mapa.py
+++ mapa.py
@@ -15,7 +15,6 @@ class Coord:
         """
         self.fila = fila
         self.columna = columna
-        self.bloqueada = False #Desbloqueada = False, Bloqueada = True
 
     def trasladar(self, df, dc):
         """Trasladar una celda.
@@ -94,6 +93,7 @@ class Mapa:
         self.columnas = columnas
         self.origen = Coord()
         self.destino = Coord(filas, columnas)
+        self.paredes = []
 
     def dimension(self):
         """Dimensiones del mapa (filas y columnas).
@@ -144,7 +144,7 @@ class Mapa:
         Devuelve:
             bool: True si la celda está bloqueada
         """
-        return coord.bloqueada
+        return coord in self.paredes
 
     def bloquear(self, coord):
         """Bloquear una celda.
@@ -154,8 +154,8 @@ class Mapa:
         Argumentos:
             coord (Coord): Coordenadas de la celda a bloquear
         """
-        if not coord.bloqueada:
-            coord.bloqueada = True
+        if coord not in self.paredes:
+            self.paredes.append(coord)
 
     def desbloquear(self, coord):
         """Desbloquear una celda.
@@ -165,8 +165,8 @@ class Mapa:
         Argumentos:
             coord (Coord): Coordenadas de la celda a desbloquear
         """
-        if coord.bloqueada:
-            coord.bloqueada = False
+        if coord in self.paredes:
+            self.paredes.remove(coord)
 
     def alternar_bloque(self, coord):
         """Alternar entre celda bloqueada y desbloqueada.
@@ -176,7 +176,10 @@ class Mapa:
         Argumentos:
             coord (Coord): Coordenadas de la celda a alternar
         """
-        coord.bloqueada = not coord.bloqueada
+        if coord in self.paredes:
+            self.paredes.remove(coord)
+        else:
+            self.paredes.append(coord)
 
     def es_coord_valida(self, coord):
         """¿Las coordenadas están dentro del mapa?
@@ -216,4 +219,21 @@ class Mapa:
             >>> for coord in mapa:
             >>>     print(coord, mapa.celda_bloqueada(coord))
         """
-        raise NotImplementedError()
+        return _Iteradormapa(self.filas, self.columnas)
+
+class _Iteradormapa:
+    def __init__(self, filas, columnas):
+        self.i = 0
+        self.j = 0
+        self.filas = filas
+        self.columnas = columnas
+    def __next__(self):
+        if self.i == self.filas:
+            raise StopIteration()
+        coord = Coord(self.i, self.j)
+        if self.j < self.columnas - 1:
+            self.j += 1
+        else:
+            self.j = 0
+            self.i += 1
+        return coord
