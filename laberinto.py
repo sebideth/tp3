03cf9d5938c542941586deef0ac796043ee5fafe
sebideth <sebaznarez@hotmail.com>
2019-11-15 14:16:25 -0300

    genera laberinto, falta que vuelva para atras

--- laberinto.py
+++ laberinto.py
@@ -1,5 +1,5 @@
 from mapa import Coord, Mapa
-
+from random import choice
 def generar_laberinto(filas, columnas):
     """Generar un laberinto.
 
@@ -11,15 +11,40 @@ def generar_laberinto(filas, columnas):
               aleatorio
     """
     mapa = Mapa(filas, columnas)
-    for i in range(columnas):
-        mapa.bloquear(Coord(0, i))
-        mapa.bloquear(Coord(filas - 1, i))
-    for i in range(filas):
-        mapa.bloquear(Coord(i, 0))
-        mapa.bloquear(Coord(i, columnas - 1))
-    for i in range(1,filas-1):
-        for j in range(1,columnas-1):
+    for i in range(0,filas):
+        for j in range(0,columnas):
             mapa.bloquear(Coord(i,j))
     mapa.asignar_origen(Coord(1,1))
     mapa.asignar_destino(Coord(filas - 2, columnas - 2))
+
+    visitadas = set()
+
+    backtrack(mapa.origen(), visitadas, mapa)
+
     return mapa
+
+def backtrack(celda, visitadas, mapa):
+    visitadas.add(celda)
+    mapa.paredes.remove(celda)
+    celdas_vecinas = buscar_celdas_vecinas(celda, mapa, visitadas)
+    if  celdas_vecinas == [] or celda == mapa.destino():
+        return
+    vecina, intermedia = definir_celda_vecina_intermedia(celda, celdas_vecinas, mapa)
+    visitadas.add(intermedia)
+    mapa.paredes.remove(intermedia)
+    backtrack(vecina, visitadas, mapa)
+
+def buscar_celdas_vecinas(celda, mapa, visitadas):
+    posibles_celdas_vecinas = [(2,0),(-2,0),(0,2),(0,-2)]
+    celdas_vecinas = []
+    for df, dc in posibles_celdas_vecinas:
+        vecina = mapa.trasladar_coord(celda, df, dc)
+        if  vecina != celda and vecina not in visitadas:
+            celdas_vecinas.append((df ,dc))
+    return celdas_vecinas
+
+def definir_celda_vecina_intermedia(celda, celdas_vecinas, mapa):
+    dist = choice(celdas_vecinas)
+    vecina =  mapa.trasladar_coord(celda, dist[0], dist[1])
+    intermedia =  mapa.trasladar_coord(celda, dist[0] // 2, dist[1] // 2)
+    return vecina, intermedia
